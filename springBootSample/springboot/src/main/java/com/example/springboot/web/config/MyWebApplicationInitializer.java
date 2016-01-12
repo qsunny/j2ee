49b31bb8b2d31a294688e9ea047c015663804e43
aaron.qiu <qsunny2004@gmail.com>
2016-01-12 13:35:10 +0800

    modify application files

--- springBootSample/springboot/src/main/java/com/example/springboot/web/config/MyWebApplicationInitializer.java
+++ springBootSample/springboot/src/main/java/com/example/springboot/web/config/MyWebApplicationInitializer.java
@@ -1,24 +1,24 @@
-package com.example.springboot.web.config;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletRegistration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.XmlWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
-@Configuration
-public class MyWebApplicationInitializer implements WebApplicationInitializer {
-
-    @Override
-    public void onStartup(ServletContext container) {
-//        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-//        appContext.setConfigLocation("/WEB-INF/springmvc-dispatcher-servlet.xml");
-
-        ServletRegistration.Dynamic registration = container.addServlet("dispatcher", new DispatcherServlet());
-        registration.setLoadOnStartup(1);
-        registration.addMapping("*.jhtml");
-    }
-
-}
+//package com.example.springboot.web.config;
+//
+//import javax.servlet.ServletContext;
+//import javax.servlet.ServletRegistration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.WebApplicationInitializer;
+//import org.springframework.web.context.support.XmlWebApplicationContext;
+//import org.springframework.web.servlet.DispatcherServlet;
+//
+//@Configuration
+//public class MyWebApplicationInitializer implements WebApplicationInitializer {
+//
+//    @Override
+//    public void onStartup(ServletContext container) {
+////        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+////        appContext.setConfigLocation("/WEB-INF/springmvc-dispatcher-servlet.xml");
+//
+//        ServletRegistration.Dynamic registration = container.addServlet("dispatcher", new DispatcherServlet());
+//        registration.setLoadOnStartup(1);
+//        registration.addMapping("*.jhtml");
+//    }
+//
+//}
