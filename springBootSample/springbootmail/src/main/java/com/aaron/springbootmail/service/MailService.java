6e3f390579180f259e094fa4f569d9e649576311
aaron.qiu <247986063@qq.com>
2017-09-03 15:49:17 +0800

    add springbootmail project test

---
+++ springBootSample/springbootmail/src/main/java/com/aaron/springbootmail/service/MailService.java
@@ -0,0 +1,14 @@
+package com.aaron.springbootmail.service;
+
+/**
+ * Created by Aaron.qiu on 2017/9/3.
+ */
+public interface MailService {
+    public void sendSimpleMail(String to, String subject, String content);
+
+    public void sendHtmlMail(String to, String subject, String content);
+
+    public void sendAttachmentsMail(String to, String subject, String content, String filePath);
+
+    public void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId);
+}
