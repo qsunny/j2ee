c53092cb69955f1bea5b108175d17c44f567dbbf
aaron.qiu <247986063@qq.com>
2017-10-29 15:07:50 +0800

    add execute build order test

---
+++ gradle/projectExecutionOrder/MainProject/src/main/java/com/test/Main.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import com.test.one.One;
+import com.test.two.Two;
+
+public class Main {
+  public static void main(String[] args) {
+	 System.out.println(One.getName()); 
+	 System.out.println(Two.getMsg()); 
+  }
+}
+ 
