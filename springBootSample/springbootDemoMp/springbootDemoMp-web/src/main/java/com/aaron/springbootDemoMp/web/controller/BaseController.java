9be095949d02761688b65a0257b7de6a3d35bb3a
aaron.qiu <aaron.qiu@yunlang.com>
2021-10-25 18:36:26 +0800

    feat:文件

--- springBootSample/springbootDemoMp/springbootDemoMp-web/src/main/java/com/aaron/springbootDemoMp/web/controller/BaseController.java
+++ springBootSample/springbootDemoMp/springbootDemoMp-web/src/main/java/com/aaron/springbootDemoMp/web/controller/BaseController.java
@@ -1,7 +1,6 @@
 package com.aaron.springbootDemoMp.web.controller;
 
 
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
